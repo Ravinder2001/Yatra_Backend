e6fe90509d7bc38ffb857a85b94fe0a1abd025a3
Ravinder Singh Negi <rvnegi786@gmail.com>
2022-01-29 19:24:27 +0530

    model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,17 @@
 const mongoose=require("mongoose")
 const userSchema = new mongoose.Schema(
-    {
-        name: {type:String},
-      father_name: {type:String},
-      email: {type:String},
-      mobile: {type:String}
-    }, {
-        versionKey:false
-    }
-)
+	{
+		email: { type: String },
+		password: { type: String },
+
+		fname: { type: String },
+		lname: { type: String },
+
+		login: { type: String },
+	},
+	{
+		versionKey: false,
+	},
+);
 const User = mongoose.model("User", userSchema)
 module.exports=User
