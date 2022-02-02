e470d53ebffb7e79c66ece4b90781b5456a2c0c8
Ravinder Singh Negi <rvnegi786@gmail.com>
2022-02-02 09:58:56 +0530

    number

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose")
 const userSchema = new mongoose.Schema(
 	{
+		mobile: { type: String, unique: true },
 		email: { type: String, unique: true },
 		password: { type: String },
 
